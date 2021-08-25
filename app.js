d191cc73633a1319bcd904bb6dac7d6e1c701f62
Demilululu <demilululu@gmail.com>
2021-08-26 00:47:08 +0800

    Add admin detail and edit function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
+const methodOverride = require('method-override')
 const flash = require('connect-flash')
 
 const passport = require('./config/passport')
@@ -12,6 +13,7 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: true }))
 app.use(passport.initialize())
 app.use(passport.session())
