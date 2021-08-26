3d20e867f9637f3ad8faad8dc3611274e755800f
Demilululu <demilululu@gmail.com>
2021-08-26 22:57:03 +0800

    Add image upload feature via multer

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 
+const fs = require('fs')
+
 const adminController = {
   // Index
   getRestaurants: (req, res) => {
@@ -15,15 +17,33 @@ const adminController = {
   },
   postRestaurant: (req, res) => {
     const { name, tel, address, opening_hours, description } = req.body
+    const { file } = req
+
     if (!name) {
       req.flash('error_messages', "name is a required field")
       return res.redirect('back')
     }
-    return Restaurant.create({ name, tel, address, opening_hours, description })
-      .then((restaurant) => {
-        req.flash('success_messages', 'restaurant has successfully been created')
-        res.redirect('/admin/restaurants')
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          return Restaurant.create({
+            name, tel, address, opening_hours, description,
+            image: file ? `/upload/${file.originalname}` : null
+          })
+            .then((restaurant) => {
+              req.flash('success_messages', 'restaurant was created successfully')
+              return res.redirect('/admin/restaurants')
+            })
+        })
       })
+    } else {
+      Restaurant.create({ name, tel, address, opening_hours, description, image: null })
+        .then((restaurant) => {
+          req.flash('success_messages', 'restaurant was created successfully')
+          res.redirect('/admin/restaurants')
+        })
+    }
   },
   // Detail
   getRestaurant: (req, res) => {
@@ -41,18 +61,38 @@ const adminController = {
   },
   putRestaurant: (req, res) => {
     const { name, tel, address, opening_hours, description } = req.body
+    const { file } = req
+
     if (!name) {
       req.flash('error_messages', "name didn't exist")
       return res.redirect('back')
     }
-    return Restaurant.findByPk(req.params.id)
-      .then((restaurant) => {
-        restaurant.update({ name, tel, address, opening_hours, description })
-          .then((restaurant) => {
-            req.flash('success_messages', 'restaurant has successfully been updated')
-            res.redirect('/admin/restaurants')
-          })
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          return Restaurant.findByPk(req.params.id)
+            .then((restaurant) => {
+              restaurant.update({
+                name, tel, address, opening_hours, description,
+                image: file ? `/upload/${file.originalname}` : restaurant.image
+              }).then((restaurant) => {
+                req.flash('success_messages', 'restaurant was successfully to update')
+                res.redirect('/admin/restaurants')
+              })
+            })
+        })
       })
+    } else {
+      return Restaurant.findByPk(req.params.id)
+        .then((restaurant) => {
+          restaurant.update({ name, tel, address, opening_hours, description, image: restaurant.image })
+            .then((restaurant) => {
+              req.flash('success_messages', 'restaurant was updated successfully ')
+              res.redirect('/admin/restaurants')
+            })
+        })
+    }
   },
   // Delete
   deleteRestaurant: (req, res) => {
@@ -60,7 +100,7 @@ const adminController = {
       .then((restaurant) => {
         restaurant.destroy()
           .then((restaurant) => {
-            req.flash('success_messages', 'restaurant has successfully been removed')
+            req.flash('success_messages', 'restaurant was removed successfully')
             res.redirect('/admin/restaurants')
           })
       })
