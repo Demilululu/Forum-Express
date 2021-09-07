c66df4cc3c6a62e0220ef208c9f5fbf1c84af789
Demilululu <demilululu@gmail.com>
2021-09-08 01:10:30 +0800

    refactor router & add GET /api/admin/restaurants

---
+++ routes/routes.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const router = express.Router();
+
+const passport = require('../config/passport')
+
+const helpers = require('../_helpers');
+
+const restController = require('../controllers/restController.js')
+const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController')
+const categoryController = require('../controllers/categoryController')
+const commentController = require('../controllers/commentController')
+
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
+
+const authenticated = (req, res, next) => {
+  if (helpers.ensureAuthenticated(req)) {
+    return next()
+  }
+  res.redirect('/signin')
+}
+const authenticatedAdmin = (req, res, next) => {
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).isAdmin) { return next() }
+    return res.redirect('/')
+  }
+  res.redirect('/signin')
+}
+
+// Users
+// signup
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+
+// signin
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
+
+// top
+router.get('/users/top', authenticated, userController.getTopUser)
+router.get('/restaurants/top', authenticated, restController.getTopRestaurant)
+
+// restaurants
+router.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+router.get('/restaurants', authenticated, restController.getRestaurants)
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
+router.get('/restaurants/:id', authenticated, restController.getRestaurant)
+router.get('/restaurants/:id/dashboard', authenticated, restController.getDashBoard)
+
+// comments
+router.post('/comments', authenticated, commentController.postComment)
+router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
+
+// favorites
+router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
+router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
+
+// like
+router.post('/like/:restaurantId', authenticated, userController.addLike)
+router.delete('/like/:restaurantId', authenticated, userController.removeLike)
+
+// followship
+router.post('/following/:userId', authenticated, userController.addFollowing)
+router.delete('/following/:userId', authenticated, userController.removeFollowing)
+
+
+// profile
+router.get('/users/:id', authenticated, userController.getUser)
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+
+// Admin
+// users
+router.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+router.get('/admin/users', authenticatedAdmin, adminController.getUsers)
+router.put('/admin/users/:id/toggleAdmin', authenticatedAdmin, adminController.toggleAdmin)
+
+// restaurants
+router.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+router.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
+router.post('/admin/restaurants', authenticatedAdmin, upload.single('image'), adminController.postRestaurant)
+router.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
+router.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
+router.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
+router.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
+
+// categories
+router.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
+router.post('/admin/categories', authenticatedAdmin, categoryController.postCategories)
+router.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
+router.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
+router.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
+
+module.exports = router
